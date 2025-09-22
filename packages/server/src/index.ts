2e4da631cd4a63d5014528c0423098e8bf2fafd3
Leya <m.shin6764@gmail.com>
2025-09-22 18:17:14 -0400

    :package: Add-server: Add helmet and cors packages

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 import about from './router/about';
 
 const CONST_HOST = 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
 app.use('/about', about);
 
