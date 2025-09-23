83a716983551b629a37eb119eaf9791aa44f7ace
Leya <m.shin6764@gmail.com>
2025-09-22 21:25:58 -0400

    :package: Add-client: Add and Setting tailwind package

--- packages/admin/eslint.config.js
+++ packages/admin/eslint.config.js
@@ -1,14 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import parser from '@typescript-eslint/parser';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    parser,
+    parserOptions: {
+      project: path.resolve(__dirname, './tsconfig.app.json'), // 여기에 실제 tsconfig 지정
+      tsconfigRootDir: __dirname,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +28,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
