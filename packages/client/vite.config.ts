c594d11e0757d8a786d925fa0eff423871e668ce
Leya <m.shin6764@gmail.com>
2025-10-01 16:17:11 -0400

    Set up  Vite alias for ui-components HMR

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // ui-components를 src로 직접 연결
+      'ui-components': path.resolve(__dirname, '../ui-components/src'),
+    },
+  },
+  server: {
+    fs: {
+      // 상위 workspace 접근 허용
+      allow: ['..'],
+    },
+  },
 });
--- packages/ui-components/src/pages/NotFoundPage.tsx
+++ packages/ui-components/src/pages/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function NotFoundPage() {
-  return <div className="bg-red-300">NotFoundPage</div>;
+  return <div className="bg-red-300">NotFoundPage!!! 우와ㅣ! ?</div>;
 }
